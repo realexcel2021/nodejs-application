e6ca2b903da0de5f60ab0546d2c9a5aded99f5b1
akurasy <akurracy@gmail.com>
2023-04-12 07:46:52 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://44.212.61.86:9000/',
+  serverUrl: 'http://34.219.93.205:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': 'd15bfaa2ea881f9088b0604bc51d9f4bbd2ea312',
+	    'sonar.projectKey':'NodejsAkuracy',
+	    'sonar.login': '95fb9c15899024ac20a0da3e5549ebb307a68e62',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
